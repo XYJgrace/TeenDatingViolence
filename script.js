a6578e7b2bebdc9006a3421c3f4c52cee2dc2650
XYJgrace <gracexiao26@gmail.com>
2024-12-29 17:28:43 -0800

    update button

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 /* .js files add interaction to your website */
-//stiky navbar
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 /*Fact Button*/
 var factList = [
